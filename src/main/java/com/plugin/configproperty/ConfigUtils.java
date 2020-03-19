78933cc37f9604f2f94d4bd29bffb7c3da0729b5
lk <likuo@richerway.cn>
2020-03-19 13:23:12 +0800

    初始化项目

--- src/main/java/com/plugin/configproperty/ConfigUtils.java
+++ src/main/java/com/plugin/configproperty/ConfigUtils.java
@@ -1,8 +1,6 @@
 package com.plugin.configproperty;
 
 import com.plugin.configproperty.spring.ConfigMsgPropertyConfigurer;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
